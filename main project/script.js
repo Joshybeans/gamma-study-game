be77ccb218ff4f9db074e8cd0ca550fb4c2a01a3
Joshybeans <redeagle9179@gmail.com>
2025-04-08 21:01:29 -0500

    commit

--- main project/script.js
+++ main project/script.js
@@ -1,3 +1,36 @@
 const canvas = document.getElementById("mainCanvas");
 const canvasCtx = canvas.getContext("2d");
+canvasCtx.fillRect(0, 0, 100, 100);
 
+/*class Vector2{
+    x;
+    y;
+    constructor(x = 0, y = 0){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Rect{
+    position;
+    size;
+    constructor(position = new Vector2(), size = new Vector2(10, 10)){
+        this.position = position;
+        this.size = size;
+    }
+    draw(){
+        canvasCtx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
+    }
+}
+
+function process(){
+    var r = new Rect();
+    r.draw();
+    setTimeout(process, 1000/60);
+}
+
+function init(){
+    canvasCtx.fillRect(0, 0, 100, 100)
+    //process();
+}
+init();*/
